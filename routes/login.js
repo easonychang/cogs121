5ad7af94ecf8d901f65a00130e76cafc2b21a206
easonychang <yuc247@ucsd.edu>
2018-04-29 19:06:26 -0700

    navbar skeletons added

--- routes/login.js
+++ routes/login.js
@@ -30,7 +30,7 @@ exports.signin =  function(req, res) {
     res.cookie(stateKey, state);
   
     // your application requests authorization
-    var scope = 'user-read-private user-read-email';
+    var scope = 'user-read-private user-read-email user-top-read user-read-recently-played';
     res.redirect('https://accounts.spotify.com/authorize?' +
       querystring.stringify({
         response_type: 'code',
@@ -51,60 +51,9 @@ exports.callback = function(req, res) {
       redirectUri: redirect_uri
     });
     
-    var scope = 'user-read-private user-read-email';
+    var scope = 'user-read-private user-read-email user-top-read';
     var state = generateRandomString(16);
   
-    //var authorizationCode = (new Buffer(client_id + ':' + client_secret).toString('base64'));
-    
-    //authorizationCode = "AQC1cW20eSVt5Aq9KcHZnG1VquZ1ufO7SWOnP_1qH0XJQpTfuv94b5wiPTjtd1OdAS607h5xFrotOVu_S4aJsZcOx-MrmR1-8-IaXbEr_f0SmqLb0qr1gOPBoR0DZ9gmTvrGozvgnT_G6I94KHTZq1YIF8ojFhlGwJvIYZWQF6sufPaMHUbpMk2d_uf7Hr0nuQwn33ZE7hD5ufMhv7-evvDsqRIQizoKaMNYUXW9V7cwFreMEDhmLg";
-
-    //console.log(authorizationCode);
-  
-    /*spotifyApi.authorizationCodeGrant(authorizationCode)
-    .then(function(data) {
-      console.log('Retrieved access token', data.body['access_token']);
-  
-      // Set the access token
-      spotifyApi.setAccessToken(data.body['access_token']);
-  
-      // Use the access token to retrieve information about the user connected to it
-      return spotifyApi.getMe();
-    })
-    .then(function(data) {
-      // "Retrieved data for Faruk Sahin"
-      console.log('Retrieved data for ' + data.body['display_name']);
-  
-      // "Email is farukemresahin@gmail.com"
-      console.log('Email is ' + data.body.email);
-  
-      // "Image URL is http://media.giphy.com/media/Aab07O5PYOmQ/giphy.gif"
-      console.log('Image URL is ' + data.body.images[0].url);
-  
-      // "This user has a premium account"
-      console.log('This user has a ' + data.body.product + ' account');
-    })
-    .catch(function(err) {
-      console.log('Something went wrong', err.message);
-    });
-  */
-  
-    /*spotifyApi.clientCredentialsGrant()
-      .then(function(data) {
-        console.log('The access token expires in ' + data.body['expires_in']);
-        console.log('The access token is ' + data.body['access_token']);
-    
-        // Save the access token so that it's used in future calls
-        spotifyApi.setAccessToken(data.body['access_token']);
-    
-        // Use the access token to retrieve information about the user connected to it
-      })
-      
-    spotifyApi.getMe()
-      .then(function(data) {
-        console.log('Some information about the authenticated user', data.body);
-      }, function(err) {
-        console.log('Something went wrong!', err);
-      });*/
     
   
     // your application requests refresh and access tokens
@@ -168,7 +117,7 @@ exports.callback = function(req, res) {
 
           spotifyApi.getMyRecentlyPlayedTracks()
             .then(function(data){
-              console.log(data);
+              console.log(data.body.items);
               
           })
           .catch(function(err) {
